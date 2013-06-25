f2989a0b03ce24f7a4597ffeddcc10b9a397bb78
da1 <dyamada1988@gmail.com>
2013-06-25 23:58:45 +0900

    description_getter.sh update

--- description_getter.sh
+++ description_getter.sh
@@ -1,4 +1,7 @@
 SCRIPT_DIR=./
 OUTPUT_FILE=./output.txt
+TMP_FILE=/tmp/tmp.txt
 
-python $SCRIPT_DIR/description_getter.py >> $OUTPUT_FILE
+python $SCRIPT_DIR/description_getter.py | sed 's/^  *//g' >> $OUTPUT_FILE
+sort $OUTPUT_FILE | uniq > $TMP_FILE
+cat $TMP_FILE > $OUTPUT_FILE
