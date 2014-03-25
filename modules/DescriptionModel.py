60fb87a82a2d4ff7b55beba297d90e879830cfe9
da1 <dyamada1988@gmail.com>
2014-03-25 23:28:24 +0900

    update

--- modules/DescriptionModel.py
+++ modules/DescriptionModel.py
@@ -2,6 +2,6 @@
 from BaseModel import BaseModel
 
 class DescriptionModel(BaseModel):
-    __descriptions = [u"だるい", u"にょわー", u"ねばねばー"]
+    __descriptions = [u"だるい", u"にょわー", u"ねばねばー", u"/usr/bin", u"/usr/local/bin"]
     def get(self):
         return self._random_pickup(self.__descriptions)
